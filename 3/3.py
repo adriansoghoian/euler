040343ab17724ddee84d166a4e8daf0086618858
adriansoghoian <adriansoghoian@gmail.com>
2014-09-03 22:41:22 -0400

    Found an inefficient solution that I need to improve...

--- 3/3.py
+++ 3/3.py
@@ -2,12 +2,14 @@
 import math
 
 number = 13195
-divisors = [1]
+divisors = []
 
 def find_primes(number):
 		for i in range(2, number):
 				if number % i == 0:
-						divisors.append(i)
-		return divisors
+						find_primes(number / i)
+		divisors.append(number)
+		return divisors[0]
 
 print(find_primes(number))
+
